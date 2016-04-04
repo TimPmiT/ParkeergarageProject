0a84ad6e471411a02e23df112d318772e14742ae
dinges <dinges@145.37.68.9>
2016-04-04 14:40:02 +0200

    Update: imports in SimulatorView verbeterd

--- ParkeergarageProject/src/SimulatorView.java
+++ ParkeergarageProject/src/SimulatorView.java
@@ -1,8 +1,7 @@
 import javax.swing.*;
-
-import SimulatorView.CarParkView;
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimulatorView extends JFrame {
     private CarParkView carParkView;
