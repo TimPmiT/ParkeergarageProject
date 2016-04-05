e9854d72a3ac8d49ff65836a45fd4f0a74ff35f6
Tim Petersen <t.petersen@st.hanze.nl>
2016-04-05 11:09:41 +0200

    TEST 1

--- ParkeergarageProject/src/Simulator.java
+++ ParkeergarageProject/src/Simulator.java
@@ -12,7 +12,7 @@ public class Simulator {
     private int hour = 0;
     private int minute = 0;
 
-    private int tickPause = 100;
+    private int tickPause = 100; //TEST TEST TEST
 
     int weekDayArrivals= 50; // average number of arriving cars per hour
     int weekendArrivals = 90; // average number of arriving cars per hour
